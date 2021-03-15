7e79d23319bcb6b4b39dad0131ae8d90a63137e5
unknown <kirtanushah@gmail.com>
2021-03-15 02:15:09 -0700

    Only require dotenv in dev environment.

--- db.js
+++ db.js
@@ -1,6 +1,5 @@
 const Pool = require("pg").Pool;
-const dotenv = require("dotenv");
-if(dotenv) dotenv.config();
+if(process.env.NODE_ENV !== "production") require("dotenv").config();
 
 const devConfig = `postgresql://${process.env.PG_USER}:${process.env.PG_PASSWORD}@${process.env.PG_HOST}:${process.env.PG_PORT}/${process.env.PG_DATABASE}`;
 const prodConfig = process.env.DATABASE_URL;

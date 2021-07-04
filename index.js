3422825c53f448afeaa042c181a97863ef175582
unknown <kirtanushah@gmail.com>
2021-07-03 22:42:18 -0700

    Add accurate timestamping

--- index.js
+++ index.js
@@ -20,14 +20,16 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, "site")))
 
 app.post('/update', async (req, res) => {
-    let timestamp = moment();
     let co2 = convert16(new Int8Array(Buffer.from(req.body.co2, 'base64')));
     let temperature = convertFloat(new Int8Array(Buffer.from(req.body.temperature, 'base64')));
-    let humidity = convertFloat(new Int8Array(Buffer.from(req.body.humidity, 'base64')));    
+    let humidity = convertFloat(new Int8Array(Buffer.from(req.body.humidity, 'base64')));
+    let timestamp = moment();
+    let timestampBegin = moment(req.body.timestamp);
+    let deltaMs = timestamp.diff(timestampBegin) / (co2.length - 1);
     let query = "INSERT INTO sensor_data VALUES ";
     for(let i = co2.length - 1; i >= 0; i--) {
         query += `('${timestamp.toISOString()}', ${co2[i]}, ${temperature[i]}, ${humidity[i]}),`;
-        timestamp.subtract(15, 's');
+        timestamp.subtract(deltaMs, 'ms');
     }
     query = query.slice(0, -1) + ";";
     const returnValue = await pool.query(query); // psql console output

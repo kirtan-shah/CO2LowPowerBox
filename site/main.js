cc735ac90b452b01a63ba28267feaef72aeccfb0
kirtan-shah <kishah@ucsd.edu>
2021-12-06 21:49:11 -0800

    Change position update to setInterval

--- site/main.js
+++ site/main.js
@@ -112,12 +112,12 @@ $(document).ready(() => {
     $('#startWindow').val(moment().subtract(1, 'd').format("YYYY-MM-DD[T]HH:mm"));
     $('#endWindow').val(moment().add(1, 'd').format("YYYY-MM-DD[T]HH:mm"));
     $('#startWindow, #endWindow').on('change', refresh);
-    setTimeout(() => {
+    setInterval(() => {
         let el = $('.infolayer .legend');
         let pos = el.offset();
         pos.top += el.find('rect').height() + 20;
         $('#picture').offset(pos);
-    }, 1000);
+    }, 500);
 })
 
 
